bde970c743c3e5cf41450f2e1fed58e79de05e11
yushoyamaguchi <ysh.824@outlook.jp>
2022-12-12 20:30:46 +0900

    parse tmpl 1

---
+++ src/calc.rs
@@ -0,0 +1,13 @@
+use crate::parser;
+use tracing::debug;
+
+pub struct Calc;
+
+impl Calc {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn run_expr(&mut self, expr: &str)  {
+    }
+}
