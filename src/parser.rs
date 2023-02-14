447df145a1c5bb509703d4d81010c344d12f5da2
yushoyamaguchi <ysh.824@outlook.jp>
2023-02-14 22:38:27 +0900

    no pest ver to main

--- src/parser.rs
+++ src/parser.rs
@@ -3,11 +3,9 @@ use pest::iterators::Pair;
  use pest_derive::Parser;
  use tracing::debug;
 
- #[derive(Parser)]
- #[grammar = "calc.pest"]
+
  struct CalcParser;
 
 pub fn parse(form1: &str) -> i32{
-    CalcParser::parse(Rule::form,form1);
     0
 }
