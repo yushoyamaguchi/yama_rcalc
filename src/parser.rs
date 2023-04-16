f112674fdf757774a960be235ae80089873ce468
yushoyamaguchi <ysh.824@outlook.jp>
2023-04-16 13:08:32 +0900

    templete of Ast

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,32 @@
 use crate::lexer::Token;
 
 
+pub struct Annot<T> {
+    value: T,
+}
+
+enum BinopKind {
+    Add,
+    Sub,
+    Mult,
+    Div, 
+}
+
+type BinOpObj=Annot<BinopKind>;
+
+enum AstKind {
+    /// 数値
+    Num(u64),
+    /// 単項演算
+    //UniOp { op: UniOp, e: Box<Ast> },
+    /// 二項演算
+    BinOp { op: BinOpObj, l: Box<Ast>, r: Box<Ast> },
+}
+
+
+
+type Ast=Annot<AstKind>;
+
 pub fn parse(tokens:Vec<Token>) -> i32{
     return 0;
 }
